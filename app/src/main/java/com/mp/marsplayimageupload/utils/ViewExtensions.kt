034441578cabcf43a8b1b61abf7fa3bc4a471f42
Prasenjit Banik <prasen267@gmail.com>
2020-05-08 15:51:21 +0530

    final commit

--- app/src/main/java/com/mp/marsplayimageupload/utils/ViewExtensions.kt
+++ app/src/main/java/com/mp/marsplayimageupload/utils/ViewExtensions.kt
@@ -1,13 +1,9 @@
 package com.mp.marsplayimageupload.utils
 
-import android.content.ContentResolver
-import android.content.Context
-import android.net.Uri
 import android.os.Build
 import android.view.DisplayCutout
 import android.view.View
 import android.view.WindowManager
-import android.webkit.MimeTypeMap
 import android.widget.ImageButton
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
@@ -42,49 +38,9 @@ fun ImageButton.simulateClick(delay: Long = ANIMATION_FAST_MILLIS) {
         isPressed = false
     }, delay)
 }
-/** Pad this view with the insets provided by the device cutout (i.e. notch) */
-@RequiresApi(Build.VERSION_CODES.P)
-fun View.padWithDisplayCutout() {
 
-    /** Helper method that applies padding from cutout's safe insets */
-    fun doPadding(cutout: DisplayCutout) = setPadding(
-        cutout.safeInsetLeft,
-        cutout.safeInsetTop,
-        cutout.safeInsetRight,
-        cutout.safeInsetBottom)
-
-    // Apply padding using the display cutout designated "safe area"
-    rootWindowInsets?.displayCutout?.let { doPadding(it) }
-
-    // Set a listener for window insets since view.rootWindowInsets may not be ready yet
-    setOnApplyWindowInsetsListener { _, insets ->
-        insets.displayCutout?.let { doPadding(it) }
-        insets
-    }
-}
 fun getFileExtension(uri:String):String{
 
     return uri.substring(uri.lastIndexOf("."))
 }
 
-/*fun getFileExtension(uri:Uri,context: Context): String? {
-    val contentResolver: ContentResolver =context.contentResolver
-    val mime: MimeTypeMap = MimeTypeMap.getSingleton()
-
-    return mime.getExtensionFromMimeType(contentResolver.getType(uri))*/
-/** Same as [AlertDialog.show] but setting immersive mode in the dialog's window */
-fun AlertDialog.showImmersive() {
-    // Set the dialog to not focusable
-    window?.setFlags(
-        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
-
-    // Make sure that the dialog's window is in full screen
-    window?.decorView?.systemUiVisibility = FLAGS_FULLSCREEN
-
-    // Show the dialog while still in immersive mode
-    show()
-
-    // Set the dialog to focusable again
-    window?.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
-}
